290ed8d476e3cf390e5e2265e976255596d95eae
ליאן בן חיים <u9200449@bsmch.net>
2023-03-05 15:58:16 +0200

    Feat (incidents.ts): Added the 'get all incidents' route

--- src/routes/incidents.ts
+++ src/routes/incidents.ts
@@ -1,5 +1,15 @@
 import express, { Request, Response, Router } from 'express';
 
+import { getAllIncidents } from '../dal/incidentDal';
+
 export const incidentsRouter: Router = express.Router();
 
-incidentsRouter.get('/', async (req: Request, res: Response) => {});
+incidentsRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const allIncidents = await getAllIncidents();
+        res.send(allIncidents);
+    } catch (error: any) {
+        console.log(error.message);
+        res.send("Error => " + error.message);
+    }
+});
