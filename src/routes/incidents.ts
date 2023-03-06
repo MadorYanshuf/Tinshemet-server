bc4d63d322c80c1f88c4f3e14572334767aa9f64
ליאן מנגד <u9254972@bsmch.net>
2023-03-06 13:56:33 +0200

    get all incidents

--- src/routes/incidents.ts
+++ src/routes/incidents.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response, Router } from 'express';
-import { getAllIncidents } from '../dal/incidentDal';
+import express, { Request, Response, Router } from "express";
+import { getAllIncidents } from "../dal/incidentDal";
 
 export const incidentsRouter: Router = express.Router();
 
-incidentsRouter.get('/', async (req: Request, res: Response) => {await res.send(await getAllIncidents())});
+incidentsRouter.get("/", async (req: Request, res: Response) =>
+  res.send(await getAllIncidents())
+);
