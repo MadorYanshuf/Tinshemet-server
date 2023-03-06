a393e3f299ba093144457aaee6a67a90319e7af2
u9074089 <u9074089@bsmch.net>
2023-03-06 14:39:40 +0200

    add (incidents.ts): added post request

--- src/routes/incidents.ts
+++ src/routes/incidents.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express';
-import { deleteIncident } from '../dal/incidentDal';
+import { addIncident, deleteIncident } from '../dal/incidentDal';
 import { getAllIncidents } from '../dal/incidentDal';
+import { Incident } from '../dal/incidentDal';
 
 export const incidentsRouter: Router = express.Router();
 
@@ -13,17 +14,28 @@ incidentsRouter.get('/', async (req: Request, res: Response) => {
         res.status(400).send('Status: Bad Request');
     }
 });
-    
+
 incidentsRouter.delete('/delete/:id', async (req: Request, res: Response) => {
-    const id : number = parseInt(req.params.id);
+    const id: number = parseInt(req.params.id);
 
     if (isNaN(id)) {
         res.status(400).send('Invalid ID');
         return;
     }
+
     try {
         await deleteIncident(id);
         res.status(200).send('Incident successfuly deleted');
+    } catch (err) {
+        res.status(404).send('Incident not found');
+    }
+});
+
+incidentsRouter.post('/add', async (req: Request, res: Response) => {
+    const incident: Incident = req.body;
+    try {
+        await addIncident(incident);
+        res.status(200).send('Incident successfuly added');
     } catch (err) {
         res.status(404).send('Incident not found');
     }
