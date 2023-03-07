a9eec9aaee619a102732209cdb574fc2f78e0ab7
u9074089 <u9074089@bsmch.net>
2023-03-07 09:14:05 +0200

    add (.env): added .env file with db related variables

--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
-
-const USERNAME: string = "tinshemetdev";
-const PASSWORD: string = "Tinshemet123!@";
-const NAME: string = "madorYanshufDev";
-const URL: string = `mongodb://10.252.30.132`;
+require("dotenv").config();
 
 export const connectToMongo = async () => {
     try {
-        await mongoose.connect(URL, {
-            user: USERNAME,
-            pass: PASSWORD,
-            dbName: NAME,
+        await mongoose.connect(process.env.URL as string, {
+            user: process.env.DB_USERNAME,
+            pass: process.env.PASSWORD,
+            dbName: process.env.NAME,
         });
         console.log("Database connection successful");
     } catch (err) {
