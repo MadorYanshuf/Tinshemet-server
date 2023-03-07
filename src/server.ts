48d329ec360ab374746ff915a1c6d66fdc270b50
עמית משולם <u9263888@bsmch.net>
2023-03-07 16:33:21 +0200

    Fix(server.ts): Hotfixes

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.set('PORT', process.env.PORT || 8000);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Get!!!');
-});
-
 app.use('/incidents', createRouter);
 
 app.listen(app.get('PORT'), () => {
@@ -21,4 +17,5 @@ app.listen(app.get('PORT'), () => {
     `⚡️[server]: Server is running at http://localhost:${app.get('PORT')}`
   );
 });
+
 connectToMongo();
