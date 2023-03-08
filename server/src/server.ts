7bbc16f019f296c08edc0f0b9303b79a50b98e88
עמית משולם <u9263888@bsmch.net>
2023-03-08 13:52:33 +0200

    Feature(server.ts): Adding sensors router

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,8 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
-import { createRouter } from './routes/incidents';
+import { incidentsRouter } from './routes/incidents';
+import { sensorsRouter } from './routes/sensors';
 import { connectToMongo } from './services/dbService';
 
 export const app: Express = express();
@@ -10,7 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.set('PORT', process.env.PORT || 8000);
 
-app.use('/incidents', createRouter);
+app.use('/incidents', incidentsRouter);
+app.use('/sensors', sensorsRouter);
 
 app.listen(app.get('PORT'), () => {
   console.log(
