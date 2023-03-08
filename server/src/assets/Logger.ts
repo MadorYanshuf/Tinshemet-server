ade4ae4d9cb75c024bb3b605dce30a5909c84d35
עמית משולם <u9263888@bsmch.net>
2023-03-08 11:19:06 +0200

    Feature(Logger.ts): Extracting logging function outside

---
+++ server/src/assets/Logger.ts
@@ -0,0 +1,6 @@
+export const logData = (funcName: any, data: Object = "None") => {
+    console.log("--------------------");
+    console.log("[Function logged]:", funcName);
+    console.log("[Data inserted]:", data);
+    console.log("--------------------");
+}
